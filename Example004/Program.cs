a451d75d895fa844cd89ec13c46d65e9ce0f5876
NatalyaGorbonosova <gorbonosova.n@mail.ru>
2022-09-02 23:06:37 +0300

    Task 4 Palindrom

---
+++ Example004/Program.cs
@@ -0,0 +1,30 @@
+﻿//Задача 19 HARD - необязательная
+//Напишите программу, которая принимает на вход любое число и проверяет, является ли оно палиндромом.
+string CheckPalindrom(string num)
+{
+    int length = num.Length;
+    int x = Convert.ToInt32(num);
+    string answer = "";
+    for(int i = 0; i < length/2; i++)
+    {
+        int x1 = (x/(Convert.ToInt32(Math.Pow(10, length - 1 - i))))%10;
+        int x2 = (x/(Convert.ToInt32(Math.Pow(10, i))))%10;
+        if(x1 == x2) answer = "Yes";
+        else
+        {
+            answer = "No";
+            break;
+        }
+    }
+    return answer;
+}
+try
+{
+    Console.Write("Введите число: ");
+    string str = Console.ReadLine();
+    Console.WriteLine(CheckPalindrom(str));
+}
+catch
+{
+    Console.WriteLine("Введите цело число!");
+}
---
+++ Example005/Program.cs
@@ -0,0 +1,38 @@
+﻿// //Задача 21 HARD - необязательная
+//Напишите программу, которая принимает на вход целое положительное число N и координаты двух точек
+// и находит расстояние между ними в N-мерном пространстве.
+double Distant3D(double [] arrayA, double [] arrayB) //Находит расстояние между точками
+{
+    double sum = 0;
+    for (int i = 0; i < arrayA.Length; i++)
+    {
+        sum = sum + Math.Pow(arrayB[i] - arrayA[i], 2);
+    }
+    double dist = Math.Sqrt(sum);
+    return Math.Round(dist, 2);
+}
+double[] FillArray(int length)
+{
+    double[] array = new double[length];
+    for(int index = 0; index < length; index++) array[index] = Convert.ToDouble(Console.ReadLine());
+    return array;
+}
+try
+{ 
+Console.WriteLine("Введите размерность пространства: ");
+int N = Convert.ToInt32(Console.ReadLine());
+double [] ArrayPointA = new double [N];
+Console.WriteLine("Введите координаты точки А: ");
+ArrayPointA = FillArray(N);
+
+double [] ArrayPointB = new double [N];
+Console.WriteLine("Введите координаты точки B: ");
+ArrayPointB = FillArray(N);
+
+Console.WriteLine("Расстояние между точками равно: " + Distant3D(ArrayPointA, ArrayPointB));
+}
+catch
+{
+    Console.WriteLine("При введении координат используйте числа!!!");
+}
+
